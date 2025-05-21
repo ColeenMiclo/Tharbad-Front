d72edf922067dc8117c7df96250e43a7a6bcd133
Coleen Miclo <coleen.miclo@gmail.com>
2025-05-21 10:40:29 +0200

    route login and register connected

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit() {
+    this.authService.login({ email: this.email, password: this.password }).subscribe({
+      next: (res) => {
+        this.authService.saveToken(res.token);
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => alert('La connexion a échoué. Veuillez vérifier vos informations')
+    });
+  }
+}
