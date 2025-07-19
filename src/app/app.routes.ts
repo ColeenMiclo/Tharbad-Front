9bf6e9571ca2e770303dc12c34515f87fe6f5f97
Coleen Miclo <coleen.miclo@gmail.com>
2025-07-19 10:21:03 +0200

    Merging all branches

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { RegisterComponent } from './pages/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'game/:id', component: GameDetailComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
